bfed93507a442f9d3c90e4484df74a6fe86f3787
Guillaume Wenzek <gwenzek@users.noreply.github.com>
2025-03-17 23:57:21 +0100

    fix main2

--- main2.zig
+++ main2.zig
@@ -6,11 +6,11 @@ const main = @import("src/main.zig");
 pub const ThreadPool = main.ThreadPool;
 pub const stream = main.stream;
 
-pub const Options = struct {
+pub const BackendOptions = struct {
     linux_backend: main.Backend = .epoll,
 };
 
-pub const options: Options = if (@hasDecl(root, "xev_options")) root.xev_options else .{};
+pub const options: BackendOptions = if (@hasDecl(root, "xev_options")) root.xev_options else .{};
 
 const default: main.Backend = switch (builtin.os.tag) {
     .ios, .macos => .kqueue,
@@ -20,36 +20,5 @@ const default: main.Backend = switch (builtin.os.tag) {
     else => @compileError("Unsupported OS"),
 };
 
-const api = default.Api();
-pub const dynamic = api.dynamic;
-pub const backend = api.backend;
-pub const available = api.available;
-pub const Loop = api.Loop;
-pub const Completion = api.Completion;
-pub const Result = api.Result;
-pub const ReadBuffer = api.ReadBuffer;
-pub const WriteBuffer = api.WriteBuffer;
-pub const Options = api.Options;
-pub const RunMode = api.RunMode;
-pub const CallbackAction = api.CallbackAction;
-pub const CompletionState = api.CompletionState;
-pub const AcceptError = api.AcceptError;
-pub const CancelError = api.CancelError;
-pub const CloseError = api.CloseError;
-pub const ConnectError = api.ConnectError;
-pub const ShutdownError = api.ShutdownError;
-pub const WriteError = api.WriteError;
-pub const ReadError = api.ReadError;
-pub const PollError = api.PollError;
-pub const PollEvent = api.PollEvent;
-pub const WriteQueue = api.WriteQueue;
-pub const WriteRequest = api.WriteRequest;
-pub const Async = api.Async;
-pub const File = api.File;
-pub const Process = api.Process;
-pub const Stream = api.Stream;
-pub const Timer = api.Timer;
-pub const TCP = api.TCP;
-pub const UDP = api.UDP;
-pub const Callback = api.Callback;
-pub const Sys = api.Sys;
+pub const DynamicXev = @import("src/dynamic.zig").Xev;
+pub const Dynamic = DynamicXev(main.Backend.candidates());
